0d0220e50ff68c5411a745dff8eb7f282588dfeb
tanjirman <tanjirmahbub07@gmail.com>
2026-05-22 21:16:45 +0600

    Changing the Base-URL in auth-client

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -3,7 +3,7 @@ import { jwtClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.BETTER_AUTH_URL || process.env.NEXT_PUBLIC_BETTER_AUTH_URL ,
+    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL ,
     plugins: [
     jwtClient() 
   ],
